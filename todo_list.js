83c7067a5ecdfb2f29a4949a0580f5fb1594acae
Joyce De La Torre & Jonathan Aguilar <pair+joycedelatorre.yon123@devbootcamp.com>
2015-04-13 12:32:06 -0700

    add todo list release1 work in progress for release2

--- todo_list.js
+++ todo_list.js
@@ -1,9 +1,34 @@
 var createTodoList = function() {
-  var todoList = {};
+  var todoList = {
+     tasks:[],
+     add: function(item){
+      this.tasks.push({description:'item', completed:false})
+     },
+     list: function(){
+       console.log(this.tasks);
+     },
+     indexOf: function(item){
+        return this.tasks.indexOf(item);
+     },
+     remove: function(index){
+         this.tasks.splice(index);
+     },
+     get: function(index){
+
+    // access the date of the array
+    // display the properties from their desired index
+      this.tasks[index].description
+
+
+     }
+  };
+
+
 
   // your code here
 
   return todoList;
+  // return groceryList;
 };
 
 
@@ -30,21 +55,22 @@ var groceryList = createTodoList();
 groceryList.add('bread');
 groceryList.add('cheese');
 groceryList.add('milk');
+// debugger;
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: false},
+// {description: 'milk', completed: false},
 // ];
 groceryList.indexOf('cheese'); //-> 1
 groceryList.get(1); //-> {description: 'cheese', completed: false}
 groceryList.complete(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'cheese', completed: true}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'cheese', completed: true},
+// {description: 'milk', completed: false},
 // ];
 groceryList.remove(1);
 groceryList.list(); //-> [
-// {description: 'bread', completed: false}, 
-// {description: 'milk', completed: false}, 
+// {description: 'bread', completed: false},
+// {description: 'milk', completed: false},
 // ];
